caa4e59da975e2f8e8cf415f2bc59451d0b66457
Shawtyy25 <abelszolnoki@gmail.com>
2023-12-14 13:31:52 +0100

    done:)

--- example.py
+++ example.py
@@ -9,10 +9,18 @@ def randomGenerator(s, e, a):
     return rndList
 
 
-        
+def isPerfect(n):
+    if n == 1 or n == 0:
+        return False
+    else:
+        divNumbers = [1]
+    for i in range(2, n, 1):
+        if n % i == 0:
+            divNumbers.append(i)
+    
+    return sum(divNumbers) == n
 
-def isPerfect():
-    pass
+    
 
 def makeNumber(text):
     
@@ -30,8 +38,27 @@ def makeNumber(text):
 startMessage = 'Kezdő érték: '
 endMessage = 'Végérték: '
 amountMessage = 'Értékek száma: '
-
+perfectNumbers = []
+perfectNumFreq = {}
 start = makeNumber(startMessage)
 end = makeNumber(endMessage)
 amount = makeNumber(amountMessage)
 
+
+randomNumbers = randomGenerator(start, end, amount)
+print(randomNumbers)
+
+for i in randomNumbers:
+    if isPerfect(i):
+        perfectNumbers.append(i)
+        
+    
+for i in perfectNumbers:
+    if i in perfectNumFreq.keys():
+        perfectNumFreq[i] += 1
+    
+    else:
+        perfectNumFreq[i] = 1
+
+for key in perfectNumFreq.keys():
+    print(f'{key}: {perfectNumFreq[key]} db')
