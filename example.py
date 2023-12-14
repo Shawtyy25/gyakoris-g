93dbbef0cfd7384463a7c0eb83b5b4942a4d4c82
Shawtyy25 <abelszolnoki@gmail.com>
2023-12-14 12:56:31 +0100

    make numbers

--- example.py
+++ example.py
@@ -1,4 +1,37 @@
-import random
+import os
+import random as rnd
+os.system('cls')
 
-print('Számok gyakorisága egy adott intervallumban')
-print('HÁLA AZ ÉGNEK MINDJÁRT CSONGETNEK')
+def randomGenerator(s, e, a):
+    rndList = []
+    for i in range(a):
+        rndList.append(rnd.randint(s, e))   
+    return rndList
+
+
+        
+
+def isPerfect():
+    pass
+
+def makeNumber(text):
+    
+    while True:
+        num = input(text)
+
+        try: 
+            num = int(num)
+            return num
+
+        except ValueError:
+            print('helytelen érték')
+
+
+startMessage = 'Kezdő érték: '
+endMessage = 'Végérték: '
+amountMessage = 'Értékek száma: '
+
+start = makeNumber(startMessage)
+end = makeNumber(endMessage)
+amount = makeNumber(amountMessage)
+
